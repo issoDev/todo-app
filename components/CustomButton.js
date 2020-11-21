3a58346b64f466970d74f61ba2f45c8c8cb699ec
issoDev <issife@hotmail.fr>
2020-11-21 14:25:18 +0100

    update file

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default function CustomButton() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.5}>
+      <View>
+        <Text> Ajouter </Text>
+      </View>
+    </TouchableOpacity>
+    
   )
 }
 
