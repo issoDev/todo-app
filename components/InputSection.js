d4ce8e9163ae10ba9a3408fbb2555daca56b495c
issoDev <issife@hotmail.fr>
2020-11-22 21:53:23 +0100

    adding a function to add a task

--- components/InputSection.js
+++ components/InputSection.js
@@ -3,14 +3,25 @@ import {View, TextInput } from 'react-native'
 import { styles } from '../styles/styles'
 import CustomButton from './CustomButton'
 
-export default function InputSection() {
+export default function InputSection({ inputText, setInputText, addTodo }) {
+  // functions
+  const newTodo = text => {
+    setInputText(text);
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput 
         style={styles.textInput}
         placeholder="ajouter une tâche ..." 
+        value={inputText}
+        onChangeText={newTodo}
       />
-      <CustomButton title="➕"></CustomButton>
+      <CustomButton 
+        title="➕"
+        inputText={inputText}
+        addTodo={addTodo}
+      />
     </View>
   )
 }
