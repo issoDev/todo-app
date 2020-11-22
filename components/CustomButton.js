e213da94e9b29861f4183095d83226988ca0f6da
issoDev <issife@hotmail.fr>
2020-11-23 00:01:31 +0100

    added button behavior

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
+import { colors } from '../styles/colors'
 import { styles } from '../styles/styles'
 
-export default function CustomButton({ title, addTodo, inputText }) {
+export default function CustomButton({ title, addTodo, inputText, disabled, color = colors.yellow }) {
+  // functions
+  const addOneTodo = () => {
+    inputText.length > 3 ? addTodo(inputText) : null;
+  }
+  const buttonColor = () => {
+    if (disabled) {
+      return colors.lightyellow;
+    } else {
+        return colors.yellow;
+    }
+  }
+
   return (
     <TouchableOpacity 
       activeOpacity={0.5}
-      onPress={() => addTodo(inputText)}
+      onPress={addOneTodo}
     >
-      <View style={styles.customButton}>
+      <View style={[styles.customButton, { backgroundColor: buttonColor() }]}>
         <Text style={styles.textCustomButton}> {title} </Text>
       </View>
     </TouchableOpacity>
-
   )
 }
 
