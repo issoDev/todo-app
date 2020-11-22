d2e6dc1f0f72b356dd99710e9ea89a4b9f4376f6
issoDev <issife@hotmail.fr>
2020-11-23 00:00:25 +0100

    added button behavior

--- components/InputSection.js
+++ components/InputSection.js
@@ -10,7 +10,7 @@ export default function InputSection({ inputText, setInputText, addTodo }) {
   }
 
   return (
-    <View style={styles.inputContainer}>
+    <View style={ styles.inputContainer }>
       <TextInput 
         style={styles.textInput}
         placeholder="ajouter une tâche ..." 
@@ -21,6 +21,7 @@ export default function InputSection({ inputText, setInputText, addTodo }) {
         title="➕"
         inputText={inputText}
         addTodo={addTodo}
+        disabled={ inputText.length <= 0 }
       />
     </View>
   )
