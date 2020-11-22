3fdd57a1dce0c96dd62279c7b3e47903e4e2e9f2
issoDev <issife@hotmail.fr>
2020-11-22 21:51:26 +0100

    adding a function to add a task

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react';
 import { FlatList, SafeAreaView } from 'react-native';
-import { styles } from './styles/styles'
-import { todolist } from './data/todolist'
+import { styles } from './styles/styles';
+import { todolist } from './data/todolist';
+import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
 
 import Header from './components/Header';
 import InputSection from './components/InputSection';
-import TodoListSection from './components/TodoListSection'
+import TodoListSection from './components/TodoListSection';
 import SearchSection from './components/SearchSection';
 
 export default function App() {
   // states
   const [todos, setTodos] = useState(todolist);
+  const [inputText, setInputText ] = useState('');
 
   //functions
-  const addTodo = () => {
-    
+  const addTodo = todo => {
+    setTodos(prev => {
+      return [...prev, { id: uuid(), todo }]
+    })
   }
 
   return (
     <SafeAreaView style={styles.appContainer}>
       <Header todosLength={todos.length} />
-      <InputSection />
+      <InputSection 
+        inputText={inputText}
+        setInputText={setInputText}
+        addTodo={addTodo} 
+      />
       <SearchSection />
       <FlatList 
         keyExtractor={item => item.id}
-        data={todolist}
+        data={todos}
         renderItem={({ item }) => {
-          return (
-            <TodoListSection todo={item} />
+          return(
+            <TodoListSection todoItem={item.todo} todoId={item.id} />
           )
         }}
       />
