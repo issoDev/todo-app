d64424423a4bed74487830b3628338a9fa09f611
issoDev <issife@hotmail.fr>
2020-11-22 21:54:21 +0100

    adding a function to a dd a task

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
 import { styles } from '../styles/styles'
 
-export default function CustomButton({ title }) {
+export default function CustomButton({ title, addTodo, inputText }) {
   return (
-    <TouchableOpacity activeOpacity={0.5}>
+    <TouchableOpacity 
+      activeOpacity={0.5}
+      onPress={() => addTodo(inputText)}
+    >
       <View style={styles.customButton}>
         <Text style={styles.textCustomButton}> {title} </Text>
       </View>
