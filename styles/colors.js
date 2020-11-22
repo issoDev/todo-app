c8180e1974c2195c76f2975de73c6ddec035c42d
issoDev <issife@hotmail.fr>
2020-11-22 22:23:46 +0100

    updated app colors

---
+++ styles/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  // App colors
+  white: '#fffffe',
+  blue: '#272343',
+  lightblue: '##bae8e8',
+  whiteblue: '#e3f6f5',
+  yellow: '#ffd803',
+  lightyellow: '#ffef9a',
+  secondary: "#22a6b3"
+}
